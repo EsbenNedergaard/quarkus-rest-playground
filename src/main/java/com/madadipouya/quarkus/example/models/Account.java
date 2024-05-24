a477d4fe392f13507f67e10f31cd71fcafacccb4
Esben <esben.nedergaard@yahoo.dk>
2024-05-24 14:14:29 +0200

    Using lombok instead

--- src/main/java/com/madadipouya/quarkus/example/models/Account.java
+++ src/main/java/com/madadipouya/quarkus/example/models/Account.java
@@ -1,10 +1,11 @@
 package com.madadipouya.quarkus.example.models;
 
-import io.quarkus.elytron.security.common.BcryptUtil;
 import io.quarkus.security.jpa.Password;
 import io.quarkus.security.jpa.Roles;
 import io.quarkus.security.jpa.UserDefinition;
 import io.quarkus.security.jpa.Username;
+import lombok.Getter;
+import lombok.Setter;
 
 import javax.json.bind.annotation.JsonbTransient;
 import javax.persistence.*;
@@ -16,6 +17,8 @@ import javax.validation.constraints.Size;
 @Entity
 @Table(name = "users") // TODO: replace
 @UserDefinition
+@Getter
+@Setter
 public class Account {
 
     @Id
@@ -49,60 +52,4 @@ public class Account {
     @Min(1)
     @Max(200)
     private int age;
-
-    public long getId() {
-        return id;
-    }
-
-    public void setId(long id) {
-        this.id = id;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public void setUsername(String username) {
-        this.username = username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = BcryptUtil.bcryptHash(password);
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public int getAge() {
-        return age;
-    }
-
-    public void setAge(int age) {
-        this.age = age;
-    }
 }
