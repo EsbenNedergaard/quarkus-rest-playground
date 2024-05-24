c11a00232051202c936060cd26204f3466586bb6
Esben <esben.nedergaard@yahoo.dk>
2024-05-24 20:04:23 +0200

    Setup exchange endpoint

---
+++ src/main/java/com/esben/kaa/quarkus/example/controller/ExchangeRateController.java
@@ -0,0 +1,42 @@
+package com.esben.kaa.quarkus.example.controller;
+
+import com.esben.kaa.quarkus.example.clients.ExchangeRateClient;
+import com.esben.kaa.quarkus.example.entities.Account;
+import jakarta.enterprise.context.RequestScoped;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import jakarta.ws.rs.core.Response;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.media.Content;
+import org.eclipse.microprofile.openapi.annotations.media.Schema;
+import org.eclipse.microprofile.openapi.annotations.parameters.Parameter;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
+
+@RequestScoped
+@Path("/exchange-rate")
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+@Tag(name = "exchangeRate", description = "Exchange rates")
+public class ExchangeRateController {
+
+    @Inject
+    @RestClient
+    ExchangeRateClient exchangeRateClient;
+
+
+    @GET
+    @Path("/dkk-usd")
+    @Operation(summary = "Gets exchange rate from DKK to USD")
+    @Produces(MediaType.APPLICATION_JSON)
+    @APIResponses(value = {
+            @APIResponse(responseCode = "200", description = "Success",
+                    content = @Content(mediaType = "application/json", schema = @Schema(implementation = Account.class)))})
+    public Response getDkkToUsdRate(@Parameter(description = "API key for accessing the exchange rate service", required = true)
+                                    @QueryParam("apiKey") String apiKey) {
+        return Response.ok(exchangeRateClient.getExchangeRate(apiKey, "DKK", "USD")).build();
+    }
+}
